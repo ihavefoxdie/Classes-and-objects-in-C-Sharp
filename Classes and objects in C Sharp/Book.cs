b3284f2ac34ad79d84109be9672a559ea3fca0d1
Vladislav Nekrasov <vladislav.nekrasov123@gmail.com>
2022-03-05 15:20:24 +0300

    Add project files.

---
+++ Classes and objects in C Sharp/Book.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_objects_in_C_Sharp
+{
+    class Book
+    {
+        private string title = "Untitled";
+        private string author = "Unnamed";
+        public string GetTitle()
+        {
+            return this.title;
+        }
+
+        public string GetAuthor()
+        {
+            return this.author;
+        }
+        
+        public void SetTitle(string arg = null)
+        {
+            if (arg == null)
+            {
+                try
+                {
+                    Console.Write("Enter the title of the book: ");
+                    this.title = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+                this.title = arg;
+        }
+
+            public void SetAuthor(string arg = null)
+        {
+            if (arg == null)
+            {
+                try
+                {
+                    Console.Write("Enter the name of the author: ");
+                    this.author = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+                this.author = arg;
+        }
+    }
+}
---
+++ Classes and objects in C Sharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_objects_in_C_Sharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Book bible = new Book();
+            bible.SetTitle();
+            bible.SetAuthor();
+
+            Console.WriteLine(bible.GetTitle());
+            Console.WriteLine(bible.GetAuthor());
+            List<Book> books = new List<Book>();
+            books.Add(new Book());
+            books[0].SetAuthor(bible.GetAuthor());
+            books[0].SetTitle(bible.GetTitle());
+            bible.SetAuthor();
+            Console.WriteLine(books[0].GetAuthor());
+            Console.WriteLine(bible.GetAuthor());
+        }
+    }
+}
